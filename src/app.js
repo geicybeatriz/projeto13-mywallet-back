92bbed8629f946eec9f0261ecae76f8c7df24ffd
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-10-06 19:19:44 -0300

    fix: authentication error

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ import homeController from "./routers/homeRouters.js";
 const app = express();
 app.use(cors());
 app.use(json());
-app.use(authRouter);
-app.use(homeController);
+app.use("/auth", authRouter);
+app.use("/myaccount", homeController);
 app.use(handleErrorsMiddleware);
 
 export default app;
